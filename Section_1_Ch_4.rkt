0178f6407fb4066e8747941a0d50bc33637f756c
Evan Jelley <jelleyevan@gmail.com>
2024-06-05 15:03:37 -0600

    Work with intervals

--- Section_1_Ch_4.rkt
+++ Section_1_Ch_4.rkt
@@ -40,4 +40,60 @@
     [on-tick traffic-light-next 1]
     [to-draw TRAFFIC-LIGHT-SCENE]))
 
-(traffic-animation "red")
+; (traffic-animation "red")
+
+; A WorldState is a Number.
+; interpretation number of pixels between the top and the UFO
+ 
+(define WIDTH 300) ; distances in terms of pixels 
+(define HEIGHT 100)
+(define CLOSE (/ HEIGHT 3))
+(define MTSCN (empty-scene WIDTH HEIGHT)) ; short for empty scene 
+(define UFO (overlay (circle 10 "solid" "green") (rectangle 50 5 "solid" "blue")))
+ 
+; WorldState -> WorldState
+(define (main y0)
+  (big-bang y0
+     [on-tick nxt]
+     [to-draw render/status]))
+ 
+; WorldState -> WorldState
+; computes next location of UFO 
+(check-expect (nxt 11) 14)
+(define (nxt y)
+  (+ y 3))
+ 
+; WorldState -> Image
+; places UFO at given height into the center of MTSCN
+(check-expect (render 11) (place-image UFO 150 11 MTSCN))
+(define (render y)
+  (place-image UFO 150 y MTSCN))
+
+
+; Exercise 52
+; [3,5] - 3,4,5
+; (3,5] - 4,5
+; [3,5) - 3,4
+; (3,5) - 4
+
+; WorldState -> Image
+; adds a status line to the scene created by render  
+ 
+(check-expect (render/status 42)
+              (place-image (text "closing in" 11 "orange")
+                           20 20
+                           (render 42)))
+ 
+(define (render/status y)
+  (place-image
+    (cond
+      [(<= 0 y CLOSE)
+       (text "descending" 11 "green")]
+      [(and (< CLOSE y) (<= y HEIGHT))
+       (text "closing in" 11 "orange")]
+      [(> y HEIGHT)
+       (text "landed" 11 "red")])
+    20 20
+    (render y)))
+
+(main 0)
